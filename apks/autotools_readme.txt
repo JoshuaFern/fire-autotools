de9c5a82cc9311f92ad6329c534a0b5d947eda8e
Software Sucks <softwaresucks@icloud.com>
2019-03-08 15:38:06 +0000

    Update fireAutotools.sh, rooting/install.sh, rooting/eu.chainfire.supersu.282.apk, apks/org.fdroid.fdroid_1005150.apk, apks/org.fdroid.fdroid.privileged_2090.apk, apks/XposedInstaller_by_dvdandroid_19_10_18.apk, apks/autotools_readme.txt files

--- apks/autotools_readme.txt
+++ apks/autotools_readme.txt
@@ -1,1 +1,4 @@
-Placeholder.
+Sources:
+FDroid: https://f-droid.org/en/packages/org.fdroid.fdroid/
+F-Droid Privileged Extension: https://f-droid.org/en/packages/org.fdroid.fdroid.privileged/
+Xposed Installer: https://forum.xda-developers.com/xposed/material-design-xposed-installer-t3137758
--- fireAutotools.sh
+++ fireAutotools.sh
@@ -1,76 +1,93 @@
-
-echo "Welcome to Stage 1: Automated Recovery"
-echo "https://www.amazon.com/gp/help/customer/display.html/ref=hp_bc_nav?ie=UTF8&nodeId=200529680"
-echo "Place the image for your device in the ./image/ folder. Do not attempt to downgrade to an older version."
-echo "Be warned that the rooting method could be patched in future versions. 5.3.6.4 is the current version."
-echo "1. Enter recovery with the power button and left volume held down with USB plugged in."
-echo "2. Select 'wipe data/factory reset' (optional)"
-echo "3. Select 'apply update from ADB'."
-
-_pause
-
-echo "Attempting to sideload..."
-echo adb sideload ./image/update-kindle-*.bin
-
-echo "Complete, select 'reboot system now'."
-echo "Do not connect your fire to wifi on reboot."
-echo "Select a passworded wifi, click cancel, and select 'NOT NOW' to continue without wifi."
-echo "Open Settings > Device Options and tap repeatedly on Serial Number until 'Developer Options' appears."
-echo "Enable ADB and check 'Always allow from this computer.' and 'OK' when prompted."
-echo "Done. Continue to Stage 2."
-
-_pause
-
-echo "Welcome to Stage 2: Automated Rooting Process"
-echo "This rooting method is reported to work on:"
-echo "Fire HD 8 8th gen (2018)"
-echo "Fire HD 8 7th gen (2017)"
-echo "Fire HD 8 6th gen (2016)"
-echo "Fire HD 10 7th gen (2017)"
-echo "Fire TV 2 (2015)"
-echo "ASUS Zenpad Z380M"
-echo "BQ Aquaris M8"
-echo "Use at your own risk."
-
-_pause
-
-echo "Copying files..."
-adb shell rm data/local/tmp/mtk-su
-adb push /rooting/arm64/su /rooting/arm64/supolicy /rooting/arm64/libsupol.so /rooting/arm64/mtk-su /data/local/tmp
-echo "Setting permissions..."
-adb shell chmod 0755 data/local/tmp/mtk-su
-echo "We're about to attempt root, sometimes this fails and you need to try it again."
-_pause
-echo "Attempting to root..."
-adb shell ./data/local/tmp/mtk-su -v && echo Done.
-_pause
-echo "Remounting /system to read/write..."
-adb shell mount -o remount -rw /system
-echo "If you saw a permission error the root failed, try again."
-_pause
-echo "Copying su binaries into /system/xbin..."
-adb shell cp /data/local/tmp/su /system/xbin/su
-adb shell cp /data/local/tmp/su /system/xbin/daemonsu
-adb shell cp /data/local/tmp/supolicy /system/xbin/
-adb shell cp /data/local/tmp/libsupol.so /system/lib/
-adb shell cp /data/local/tmp/libsupol.so /system/lib64/
-echo "Setting permissions..."
-adb shell chmod 0755 /system/xbin/su
-adb shell chcon u:object_r:system_file:s0/system/xbin/su
-adb shell chmod 0755 /system/xbin/daemonsu
-adb shell chcon u:object_r:system_file:s0 /system/xbin/daemonsu
-echo "Starting daemonsu..."
-adb shell daemonsu --auto-daemon
-
-echo "We're about to install SuperSU, exit at this point if you want to install it or something else yourself."
-_pause
-echo "Installing SuperSU..."
-adb install SuperSU.apk
-echo "Open SuperSU app and update the binary reboot when prompted."
-echo "Set SuperSU permission mode to 'Grant' the default prompts don't work on FireOS."
-
-_pause
+#!/bin/bash
 
 _pause(){
- read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+    read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+    }
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo "       Fire Autotools"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "Stage 1: Automated Recovery"
+	echo "Stage 2: Automated Rooting"
+	echo "      3: Exit"
+	echo " "
 }
+
+# ----------------------------------------------
+# Stage #1: Automated Recovery
+# ----------------------------------------------
+one(){
+echo "${bold}Welcome to Stage 1: Automated Recovery${normal}"
+echo "${bold}https://www.amazon.com/gp/help/customer/display.html/ref=hp_bc_nav?ie=UTF8&nodeId=200529680${normal}"
+echo "${bold}Place the image for your device in the ./image/ folder. Do not attempt to downgrade to an older version.${normal}"
+echo "${bold}Be warned that the rooting method could be patched in future versions. 5.3.6.4 is the current version.${normal}"
+echo "${bold}1. Enter recovery with the power button and left volume held down with USB plugged in.${normal}"
+echo "${bold}2. Select 'wipe data/factory reset' (optional)${normal}"
+echo "${bold}3. Select 'apply update from ADB'.${normal}"
+_pause
+echo "${bold}Attempting to sideload...${normal}"
+echo adb sideload ./image/update-kindle-*.bin
+echo "${bold}Complete, select 'reboot system now'.${normal}"
+echo "${bold}Do not connect your fire to wifi on reboot.${normal}"
+echo "${bold}Select a passworded wifi, click cancel, and select 'NOT NOW' to continue without wifi.${normal}"
+echo "${bold}Open Settings > Device Options and tap repeatedly on Serial Number until 'Developer Options' appears.${normal}"
+echo "${bold}Enable ADB and check 'Always allow from this computer.' and 'OK' when prompted.${normal}"
+echo "${bold}Done. Continue to Stage 2.${normal}"
+_pause
+}
+
+# ----------------------------------------------
+# Stage #2: Automated Rooting
+# ----------------------------------------------
+two(){
+echo "${bold}Welcome to Stage 2: Automated Rooting Process${normal}"
+echo "${bold}This rooting method is reported to work on:${normal}"
+echo "${bold}Fire HD 8 8th gen (2018)${normal}"
+echo "${bold}Fire HD 8 7th gen (2017)${normal}"
+echo "${bold}Fire HD 8 6th gen (2016)${normal}"
+echo "${bold}Fire HD 10 7th gen (2017)${normal}"
+echo "${bold}Fire TV 2 (2015)${normal}"
+echo "${bold}ASUS Zenpad Z380M${normal}"
+echo "${bold}BQ Aquaris M8${normal}"
+echo "${bold}Use at your own risk.${normal}"
+_pause
+adb shell rm data/local/tmp/mtk-su
+adb push ./rooting/arm64/su ./rooting/arm64/supolicy ./rooting/arm64/libsupol.so ./rooting/arm64/mtk-su ./rooting/install.sh /data/local/tmp
+adb shell chmod 0755 /data/local/tmp/mtk-su /data/local/tmp/install.sh
+echo "${bold}Attempting to root...${normal}"
+echo "${bold}You should end up at a root command prompt, execute this command:${normal}"
+echo "${bold}exec /data/local/tmp/install.sh${normal}"
+adb shell ./data/local/tmp/mtk-su
+echo "${bold}If you saw permission errors the root failed, try again from Stage 2.${normal}"
+echo "${bold}We're about to install the SuperSU apk, exit at this point if you want to install something yourself.${normal}"
+_pause
+echo "${bold}Installing SuperSU...${normal}"
+adb install ./rooting/eu.chainfire.supersu.282.apk
+echo "${bold}Open SuperSU app and update the binary, reboot when prompted.${normal}"
+echo "${bold}Set SuperSU permission mode to 'Grant' the default prompts don't work on FireOS.${normal}"
+echo "${bold}Done. Continue to Stage 3.${normal}"
+_pause
+}
+
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 3] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) exit 0;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+
+while true
+do
+ 
+	show_menus
+	read_options
+done
