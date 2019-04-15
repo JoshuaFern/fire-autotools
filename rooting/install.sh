c8c4e2858d8ad580e4f40bc5f3d1232c92092929
Software Sucks <2916680-SoftwareSucks@users.noreply.gitlab.com>
2019-04-15 12:51:18 +0000

    Update rooting/install.sh

--- rooting/install.sh
+++ rooting/install.sh
@@ -9,9 +9,14 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-echo "Remounting /system read/write..."
+retry_(){
+echo 0 >> /sdcard/retry_
+echo "Retrying..."
+exit
+}
+rm /sdcard/retry_ 2>/dev/null
 mount -o remount -rw /system
-[ -w /system ] && echo "Mounted. Copying files..." || exit
+[ -w /system ] && echo "Mounted. Copying files..." || retry_
 cp /data/local/tmp/su /system/xbin/su
 cp /data/local/tmp/su /system/xbin/daemonsu
 cp /data/local/tmp/supolicy /system/xbin/
@@ -22,6 +27,10 @@ chmod 0755 /system/xbin/su
 chcon u:object_r:system_file:s0 /system/xbin/su
 chmod 0755 /system/xbin/daemonsu
 chcon u:object_r:system_file:s0 /system/xbin/daemonsu
-daemonsu --auto-daemon &
-echo "Success..."
+clear
+echo "Congratulations! Rooting successful!"
+echo "Open SuperSU app and update the binary using normal mode, you don't have to reboot when prompted.${normal}"
+echo "Set SuperSU default access mode to 'Grant' the prompt mode doesn't work on FireOS.${normal}"
+echo "Done. Press CTRL+C to exit and start Stage 3: Block Amazon Spying."
+daemonsu --auto-daemon
 exit
